d5d31c59b3c48c448a3f6d6c867c310800246918
habduljal <habduljal@gmail.com>
2026-03-08 04:04:33 +0300

    create login service

---
+++ back-end/src/services/login.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"social-network/src/dto"
+	"social-network/src/models"
+	"social-network/src/repo"
+	"social-network/src/utils"
+	"errors"
+	"time"
+)
+
+func LoginUser(req dto.LoginRequest) (string, string, time.Time, error) {
+	 
+
+	user, err := repo.CheckUserCredentials(req.NicknameOrEmail)
+	if err != nil {
+		return "", "", time.Time{}, err
+	}
+
+	if !utils.CheckPasswordHash(req.Password, user.PasswordHash) {
+		return "", "", time.Time{}, errors.New("invalid credentials")
+	}
+
+	sessionID, err := utils.GenerateSessionID()
+	if err != nil {
+		return "", "", time.Time{}, err
+	}
+
+    sessionExpiration := utils.GetSessionExpiry()
+    session := models.Session{
+		ID: sessionID,
+		UserID: user.ID,
+		ExpiresAt: sessionExpiration,
+	}
+	err = repo.CreateSession(session)
+	if err != nil {
+		return "", "", time.Time{}, err
+	}
+
+	return session.ID, session.UserID, session.ExpiresAt, nil
+}
