22ff94eafa0e82ae436ae8a882687008fdce4c84
habduljal <habduljal@gmail.com>
2026-03-08 04:04:08 +0300

    create a login repo functions that check thhe user and create session

--- back-end/src/repo/auth.go
+++ back-end/src/repo/auth.go
@@ -1,29 +1,30 @@
-package  repo
+package repo
+
 import (
-	
-	"social-network/src/db/sqlite"
-	"social-network/src/models"
 	"errors"
+	database "social-network/src/db/sqlite"
+	"social-network/src/models"
 )
-func RegisterUser(user models.User)   error {
 
-exists, err := CheckEmailExists( user.Email)
-if err != nil {
-	return  err
+func RegisterUser(user models.User) error {
+
+	exists, err := CheckEmailExists(user.Email)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return errors.New("email already exists")
+	}
+
+	_, err = database.DB.Exec(RegisterUserQuery, user.ID, user.Email, user.PasswordHash, user.FirstName, user.LastName, user.DateOfBirth, user.AvatarPath, user.Nickname, user.AboutMe, user.IsPublic, user.Role, user.Status, user.CreatedAt, user.UpdatedAt)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
-if exists {
-	return  errors.New("email already exists")
-}
 
-_ ,err = database.DB.Exec(RegisterUserQuery,user.ID,user.Email,user.PasswordHash,user.FirstName,user.LastName,user.DateOfBirth,user.AvatarPath,user.Nickname,user.AboutMe,user.IsPublic,user.Role,user.Status,user.CreatedAt,user.UpdatedAt,)
-if err != nil {
-	return err
-}
-
-return  nil
-} 
-
-func CheckEmailExists( email string) (bool, error) {
+func CheckEmailExists(email string) (bool, error) {
 
 	var count int
 
@@ -33,4 +34,20 @@ func CheckEmailExists( email string) (bool, error) {
 	}
 
 	return count > 0, nil
+}
+
+func CheckUserCredentials(nicknameOrEmail string) (*models.User, error) {
+	var user models.User
+
+	err := database.DB.QueryRow(CheckCredentialsQuery, nicknameOrEmail, nicknameOrEmail).Scan(&user.ID, &user.PasswordHash)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func CreateSession(session models.Session) error {
+	_, err := database.DB.Exec(CreateSessionQuery, session.ID, session.UserID, session.ExpiresAt)
+	return err
 }
