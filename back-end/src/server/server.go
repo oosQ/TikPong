9df1c1465b1414f4433b664298900aeccb06e7e2
habduljal <habduljal@gmail.com>
2026-03-08 05:45:41 +0300

    put logout endpoint

--- back-end/src/server/server.go
+++ back-end/src/server/server.go
@@ -11,10 +11,19 @@ func Serverinit() {
 	defer database.DB.Close()
 
 	http.HandleFunc("/api/auth/users", handlers.RegisterHandler)
-	http.HandleFunc("/api/auth/sessions", handlers.LoginHandler)
-
+	http.HandleFunc("/api/auth/sessions", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handlers.LoginHandler(w, r)
+		} else if r.Method == http.MethodDelete {
+			handlers.LogoutHandler(w, r)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+	
 	log.Println("Server starting on: http://localhost:8433/")
-    err := http.ListenAndServe(":8433", nil); if err != nil {
+	err := http.ListenAndServe(":8433", nil)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
