e4a4ec07efce75bdd56f83c614515dc69cd7d190
habduljal <habduljal@gmail.com>
2026-03-10 11:57:04 +0300

    add reset password endpoint

--- back-end/src/app/auth/routes.go
+++ back-end/src/app/auth/routes.go
@@ -9,6 +9,7 @@ import (
 func Init() {
 	http.HandleFunc("/api/auth/users", handlers.RegisterHandler)
 	http.HandleFunc("/api/auth/me", middleware.RequireAuth(handlers.GetCurrentUserHandler))
+	http.HandleFunc("/api/auth/reset-password",middleware.RequireAuth(handlers.ResetPasswordHandler))
 	http.HandleFunc("/api/auth/sessions", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
 			handlers.LoginHandler(w, r)
