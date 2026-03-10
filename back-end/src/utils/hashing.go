6d2c399f32d2e424926bbee2bce5117c93f8c7f9
habduljal <habduljal@gmail.com>
2026-03-11 00:47:25 +0300

    edit the bcrypt hashing cost to 12

--- back-end/src/utils/hashing.go
+++ back-end/src/utils/hashing.go
@@ -1,9 +1,9 @@
 package utils
 
 import "golang.org/x/crypto/bcrypt"
-
+ 
 func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
 		return "", err
 	}
