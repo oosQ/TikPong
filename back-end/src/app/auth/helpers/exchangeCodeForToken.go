9e15a58c4857ea0590e38c9328d855581c8928e0
habduljal <habduljal@gmail.com>
2026-03-11 09:16:41 +0300

    create Exchange Code For Token  function

---
+++ back-end/src/app/auth/helpers/exchangeCodeForToken.go
@@ -0,0 +1,48 @@
+package helpers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"social-network/src/app/auth/dto"
+	"social-network/src/utils"
+)
+func ExchangeCodeForToken(code string) (*dto.TokenResponse, error) {
+	clientID := utils.GetGoogleClientID()
+	clientSecret :=  utils.GetGoogleClientSecret()
+	redirectURI := utils.GetGoogleRedirectURI()
+
+	if clientID == "" || clientSecret == "" || redirectURI == "" {
+		return nil, errors.New("google oauth config is missing; set GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, and GOOGLE_REDIRECT_URI")
+	}
+
+	data := url.Values{}
+	data.Set("code", code)
+	data.Set("client_id", clientID)
+	data.Set("client_secret", clientSecret)
+	data.Set("redirect_uri", redirectURI)
+	data.Set("grant_type", "authorization_code")
+
+	resp, err := http.PostForm("https://oauth2.googleapis.com/token", data)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
+		return nil, fmt.Errorf("google token exchange failed: status %d", resp.StatusCode)
+	}
+
+	var tokenResp dto.TokenResponse
+	if err := json.NewDecoder(resp.Body).Decode(&tokenResp); err != nil {
+		return nil, err
+	}
+
+	if tokenResp.AccessToken == "" {
+		return nil, errors.New("google token exchange failed: missing access token in response")
+	}
+
+	return &tokenResp, nil
+}
