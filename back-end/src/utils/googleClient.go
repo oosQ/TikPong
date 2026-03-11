06b08cdacb3c60f2e80cc0ad0e476072d69f4008
habduljal <habduljal@gmail.com>
2026-03-11 09:16:09 +0300

    create functions to get GoogleClientSecret,GoogleClientID and GoogleRedirectURI

---
+++ back-end/src/utils/googleClient.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"os"
+	"strings"
+)
+
+var (
+	// These default values should be replaced with your actual Google OAuth credentials
+	defaultGoogleClientID     = ""
+	defaultGoogleRedirectURI  = "http://localhost:8433/api/auth/google/callback"
+	defaultGoogleClientSecret = ""
+)
+
+func GetGoogleClientSecret() string {
+	if value := strings.TrimSpace(os.Getenv("GOOGLE_CLIENT_SECRET")); value != "" {
+		return value
+	}
+	return defaultGoogleClientSecret
+}
+func GetGoogleClientID() string {
+	if value := strings.TrimSpace(os.Getenv("GOOGLE_CLIENT_ID")); value != "" {
+		return value
+	}
+	return defaultGoogleClientID
+}
+
+func GetGoogleRedirectURI() string {
+	if value := strings.TrimSpace(os.Getenv("GOOGLE_REDIRECT_URI")); value != "" {
+		return value
+	}
+	return defaultGoogleRedirectURI
+}
