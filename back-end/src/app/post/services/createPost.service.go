b01126a8c73d07a2bd05ef29203a58c2d5afaa55
habduljal <habduljal@gmail.com>
2026-03-17 09:59:45 +0300

    create post service

---
+++ back-end/src/app/post/services/createPost.service.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"errors"
+	"social-network/src/app/post/dto"
+	"social-network/src/app/post/repo"
+	"social-network/src/models"
+	"social-network/src/utils"
+)
+
+func CreatePost(userID string, req dto.CreatePostRequest) error {
+
+	postID, err := utils.GenerateUUID()
+	if err != nil {
+		return errors.New("Failed to generate post ID")
+	}
+
+	post := models.Post{
+		ID:        postID,
+		UserID:    userID,
+		Title:     req.Title,
+		Content:   req.Content,
+		Privacy:   req.Privacy,
+		ImagePath: req.ImagePath,
+	}
+	err = repo.CreatePost(post, req.Hashtags)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
