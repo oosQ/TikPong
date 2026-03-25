59108c66b60a659dc89c54de21d05552bb353714
habduljal <habduljal@gmail.com>
2026-03-25 09:00:07 +0300

    create and init follow routes

---
+++ back-end/src/app/user/follow/routes.go
@@ -0,0 +1,99 @@
+package follow
+
+import (
+	"net/http"
+	"social-network/src/app/user/follow/handlers"
+	"social-network/src/middleware"
+	"social-network/src/utils"
+)
+
+func Init() {
+
+	http.HandleFunc("/api/follows/{userId}", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			handlers.FollowUserHandler(w, r)
+		case http.MethodDelete:
+			handlers.UnfollowUserHandler(w, r)
+		default:
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/followers/{userId}", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodDelete {
+			handlers.RemoveFollowerHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/follow-requests", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetFollowRequestsHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/follow-requests/{userId}", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			handlers.CreateFollowRequestHandler(w, r)
+		case http.MethodDelete:
+			handlers.CancelFollowRequestHandler(w, r)
+		default:
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/follow-requests/{userId}/accept", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handlers.AcceptFollowRequestHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/follow-requests/{userId}/reject", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handlers.RejectFollowRequestHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/following", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetFollowingHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/followers", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetFollowersHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/follow-requests/sent", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetSentFollowRequestsHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+}
--- back-end/src/server/server.go
+++ back-end/src/server/server.go
@@ -1,19 +1,22 @@
 package server
+
 import (
 	"log"
 	"net/http"
-	"social-network/src/db"
-	"social-network/src/app/auth"
-	"social-network/src/app/post/core"
+	"social-network/src/app/user/follow"
+	post "social-network/src/app/post/core"
+	"social-network/src/app/user/auth"
+	database "social-network/src/db"
 )
 
 func Serverinit() {
 	database.InitDB()
 	defer database.DB.Close()
 
-	auth.Init();
-	post.Init();
-	
+	auth.Init()
+	post.Init()
+	follow.Init()
+
 	log.Println("Server starting on: http://localhost:8433/")
 	err := http.ListenAndServe(":8433", nil)
 	if err != nil {
