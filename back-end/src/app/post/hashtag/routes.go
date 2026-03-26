9f5c22a6e3fc4e768fbb1c035ea7b11fcbfab8d2
habduljal <habduljal@gmail.com>
2026-03-26 17:34:47 +0300

    create hashtag route

---
+++ back-end/src/app/post/hashtag/routes.go
@@ -0,0 +1,28 @@
+package hashtag
+
+import (
+	"net/http"
+	"social-network/src/app/post/hashtag/handlers"
+	"social-network/src/middleware"
+	"social-network/src/utils"
+)
+
+func Init() {
+	http.HandleFunc("/api/hashtags", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetAllHashtagsHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/hashtags/{hashtagId}/posts", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetPostsByHashtagHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+}
