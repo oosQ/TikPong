aa8ce770f17723efa5dd41fb2587e97fc4be0c87
habduljal <habduljal@gmail.com>
2026-03-26 15:56:38 +0300

    create comment routes

---
+++ back-end/src/app/post/comment/routes.go
@@ -0,0 +1,55 @@
+package comment
+
+import (
+	"net/http"
+	"social-network/src/app/post/comment/handlers"
+	"social-network/src/middleware"
+	"social-network/src/utils"
+)
+
+func Init() {
+	http.HandleFunc("/api/posts/{postId}/comments", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			handlers.CreateCommentHandler(w, r)
+		case http.MethodGet:
+			handlers.GetCommentsHandler(w, r)
+		default:
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/comments/{commentId}", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPatch:
+			handlers.EditCommentHandler(w, r)
+		case http.MethodDelete:
+			handlers.DeleteCommentHandler(w, r)
+		default:
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/comments/{commentId}/like", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			handlers.LikeCommentHandler(w, r)
+		case http.MethodDelete:
+			handlers.UnlikeCommentHandler(w, r)
+		default:
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+
+	http.HandleFunc("/api/users/{userId}/comments", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetUserCommentsHandler(w, r)
+		} else {
+			utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}))
+}
