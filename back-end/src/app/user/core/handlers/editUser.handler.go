b2e4e2376d09f95dfc40b0c81df3f3b6c38c05f1
habduljal <habduljal@gmail.com>
2026-03-28 19:07:34 +0300

    create edit user info handler and service

---
+++ back-end/src/app/user/core/handlers/editUser.handler.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"social-network/src/app/user/core/dto"
+	"social-network/src/app/user/core/services"
+	"social-network/src/models"
+	"social-network/src/utils"
+)
+
+func EditUserHandler(w http.ResponseWriter, r *http.Request) {
+	userCtx, ok := r.Context().Value("user_data").(*models.UserContext)
+	if !ok || userCtx == nil {
+		utils.SendError(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var req dto.EditUserRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		utils.SendError(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if err := services.EditUser(userCtx.ID, req); err != nil {
+		utils.SendError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	utils.SendSuccess(w, nil, "User updated successfully")
+}
---
+++ back-end/src/app/user/core/services/editUser.service.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"errors"
+	"social-network/src/app/user/core/dto"
+	"social-network/src/app/user/core/repo"
+)
+
+func EditUser(userID string, req dto.EditUserRequest) error {
+	user, err := repo.GetUserByID(userID)
+	if err != nil {
+		return err
+	}
+	if user == nil {
+		return errors.New("user not found")
+	}
+
+	nickname := user.Nickname
+	firstName := user.FirstName
+	lastName := user.LastName
+	aboutMe := user.AboutMe
+	isPublic := user.IsPublic
+
+	if req.Nickname != "" {
+		nickname = req.Nickname
+	}
+	if req.FirstName != "" {
+		firstName = req.FirstName
+	}
+	if req.LastName != "" {
+		lastName = req.LastName
+	}
+	if req.AboutMe != "" {
+		aboutMe = req.AboutMe
+	}
+	if req.IsPublic != nil {
+		isPublic = *req.IsPublic
+	}
+
+	return repo.UpdateUser(userID, nickname, firstName, lastName, aboutMe, isPublic)
+}
