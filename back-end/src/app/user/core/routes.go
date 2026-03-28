83c07723356be11dc94d66b0d1496e8c84a4c08f
habduljal <habduljal@gmail.com>
2026-03-28 19:09:26 +0300

    create user feature routes

---
+++ back-end/src/app/user/core/routes.go
@@ -0,0 +1,14 @@
+package core
+
+import (
+	"net/http"
+	"social-network/src/app/user/core/handlers"
+	"social-network/src/middleware"
+)
+
+func Init() {
+	http.HandleFunc("GET /api/users/me", middleware.RequireAuth(handlers.GetMeHandler))
+	http.HandleFunc("GET /api/users/{userId}", middleware.RequireAuth(handlers.GetUserHandler))
+	http.HandleFunc("PATCH /api/users/me", middleware.RequireAuth(handlers.EditUserHandler))
+	http.HandleFunc("PATCH /api/users/me/avatar", middleware.RequireAuth(handlers.ChangeAvatarHandler))
+}
