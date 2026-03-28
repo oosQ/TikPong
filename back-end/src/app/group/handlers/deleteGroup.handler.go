6764cc01232a5717db69271eed3f2ae672584afc
habduljal <habduljal@gmail.com>
2026-03-28 19:22:24 +0300

    create delete group handler

---
+++ back-end/src/app/group/handlers/deleteGroup.handler.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+	"social-network/src/app/group/services"
+	"social-network/src/models"
+	"social-network/src/utils"
+)
+
+func DeleteGroupHandler(w http.ResponseWriter, r *http.Request) {
+	userCtx, ok := r.Context().Value("user_data").(*models.UserContext)
+	if !ok || userCtx == nil {
+		utils.SendError(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	groupID := r.PathValue("groupId")
+	if groupID == "" {
+		utils.SendError(w, "Missing required path param: groupId", http.StatusBadRequest)
+		return
+	}
+
+	if err := services.DeleteGroup(groupID, userCtx.ID); err != nil {
+		utils.SendError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	utils.SendSuccess(w, nil, "Group deleted successfully")
+}
