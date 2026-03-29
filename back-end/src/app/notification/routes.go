13ce2aec039083f3ef434971b807a6f9f53b3717
habduljal <habduljal@gmail.com>
2026-03-29 23:25:45 +0300

    create notification routes

---
+++ back-end/src/app/notification/routes.go
@@ -0,0 +1,34 @@
+package notification
+
+import (
+	"net/http"
+	"social-network/src/app/notification/handlers"
+	"social-network/src/middleware"
+	"social-network/src/utils"
+)
+
+func Init() {
+	http.HandleFunc("/api/notifications", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			handlers.GetNotificationsHandler(w, r)
+			return
+		}
+		utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}))
+
+	http.HandleFunc("/api/notifications/{notificationId}/read", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handlers.MarkNotificationReadHandler(w, r)
+			return
+		}
+		utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}))
+
+	http.HandleFunc("/api/notifications/read-all", middleware.RequireAuth(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handlers.MarkAllNotificationsReadHandler(w, r)
+			return
+		}
+		utils.SendError(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}))
+}
