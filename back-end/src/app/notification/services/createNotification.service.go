c33937f0a8d7fc543b08ac6ccda09ede1a9d74b6
habduljal <habduljal@gmail.com>
2026-03-29 23:24:42 +0300

    create create-notification service

---
+++ back-end/src/app/notification/services/createNotification.service.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"encoding/json"
+	"social-network/src/app/notification/repo"
+	"social-network/src/utils"
+	"social-network/src/ws"
+)
+
+func CreateAndDispatch(userID, notificationType, title, message string, payload any) error {
+	notificationID, err := utils.GenerateUUID()
+	if err != nil {
+		return err
+	}
+
+	var payloadText *string
+	if payload != nil {
+		encoded, err := json.Marshal(payload)
+		if err != nil {
+			return err
+		}
+		text := string(encoded)
+		payloadText = &text
+	}
+
+	err = repo.CreateNotification(notificationID, userID, notificationType, title, message, payloadText)
+	if err != nil {
+		return err
+	}
+
+	ws.GlobalHub().SendToUser(userID, "notification:new", map[string]any{
+		"id":      notificationID,
+		"type":    notificationType,
+		"title":   title,
+		"message": message,
+		"payload": payload,
+	})
+
+	return nil
+}
