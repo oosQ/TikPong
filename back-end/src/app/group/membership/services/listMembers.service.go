b51ab88b63e9e74ae67ffbb0d1e081e06ec2cd74
habduljal <habduljal@gmail.com>
2026-04-02 14:35:22 +0300

    create list-members feature

---
+++ back-end/src/app/group/membership/services/listMembers.service.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"errors"
+	"social-network/src/app/group/membership/repo"
+	"social-network/src/app/group/membership/dto"
+	"social-network/src/app/group/shared"
+)
+func ListMembers(groupID, userID string) ([]dto.GroupMemberResponse, error) {
+	member, err := shared.IsMember(groupID, userID)
+	if err != nil {
+		return nil, err
+	}
+	if !member {
+		return nil, errors.New("only group members can list members")
+	}
+
+	return repo.ListMembers(groupID)
+}
+
