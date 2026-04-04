e59fb6219457429b86e50bb4203bd96ff63e3f2d
habduljal <habduljal@gmail.com>
2026-04-04 15:21:22 +0300

    add search and users endpoints

--- back-end/src/app/user/core/routes.go
+++ back-end/src/app/user/core/routes.go
@@ -8,6 +8,8 @@ import (
 
 func Init() {
 	http.HandleFunc("GET /api/users/me", middleware.RequireAuth(handlers.GetMeHandler))
+	http.HandleFunc("GET /api/users", middleware.RequireAuth(handlers.GetUsersHandler))
+	http.HandleFunc("GET /api/users/search", middleware.RequireAuth(handlers.SearchUsersHandler))
 	http.HandleFunc("GET /api/users/{userId}", middleware.RequireAuth(handlers.GetUserHandler))
 	http.HandleFunc("PATCH /api/users/me", middleware.RequireAuth(handlers.EditUserHandler))
 	http.HandleFunc("PATCH /api/users/me/avatar", middleware.RequireAuth(handlers.ChangeAvatarHandler))
