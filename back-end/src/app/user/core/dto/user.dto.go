70812c0b3d29f06d2e022308da684bcb4650c081
habduljal <habduljal@gmail.com>
2026-04-04 15:22:22 +0300

    edit the user profile dto and create search and users dto

--- back-end/src/app/user/core/dto/user.dto.go
+++ back-end/src/app/user/core/dto/user.dto.go
@@ -10,6 +10,7 @@ type UserProfileResponse struct {
 	AboutMe        string    `json:"about_me"`
 	AvatarPath     string    `json:"avatar_path"`
 	IsPublic       bool      `json:"is_public"`
+	IsFollowing    int       `json:"is_following"`
 	TotalPosts     int       `json:"total_posts"`
 	TotalFollowers int       `json:"total_followers"`
 	TotalFollowing int       `json:"total_following"`
@@ -22,4 +23,32 @@ type EditUserRequest struct {
 	LastName  string `json:"last_name"`
 	AboutMe   string `json:"about_me"`
 	IsPublic  *bool  `json:"is_public"`
+}
+
+type UserSearchResult struct {
+	ID         string `json:"id"`
+	Nickname   string `json:"nickname"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	AvatarPath string `json:"avatar_path"`
+	IsPublic   bool   `json:"is_public"`
+}
+
+type UserListItem struct {
+	ID          string `json:"id"`
+	Nickname    string `json:"nickname"`
+	AvatarPath  string `json:"avatar_path"`
+	IsFollowing int    `json:"is_following"`
+}
+
+type SearchUsersResponse struct {
+	Users      []UserSearchResult `json:"users"`
+	NextCursor string             `json:"next_cursor,omitempty"`
+	Limit      int                `json:"limit"`
+}
+
+type GetUsersResponse struct {
+	Users      []UserListItem `json:"users"`
+	NextCursor string         `json:"next_cursor,omitempty"`
+	Limit      int            `json:"limit"`
 }
