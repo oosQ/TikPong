82746d8e78c4547b7a9467203d580a9c8ab0fcc6
habduljal <zenod0nt@gmail.com>
2026-07-12 23:05:57 +0300

    inhance the docker

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import AppShell from "@/app/_components/app-shell";
 
@@ -24,7 +25,9 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <AppShell>{children}</AppShell>
+        <Suspense>
+          <AppShell>{children}</AppShell>
+        </Suspense>
       </body>
     </html>
   );
